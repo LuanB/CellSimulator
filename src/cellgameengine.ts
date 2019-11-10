096e2cae35bb57390e2fbfcdbc8518583f972e1d
Luan <luan.b@stjamesperth.com.au>
2019-11-10 14:16:06 +1100

    calculating live neighbours using Golan Levins modulus for wrap around of the board

--- src/cellgameengine.ts
+++ src/cellgameengine.ts
@@ -8,9 +8,11 @@ export type Grid = boolean[][];
 // Game engine to hold game rules and logic
 
 export class CellSimulatorEngine {
-  // We need two grids so that we can generate the generation 2 grid
-  // from calucations of generation 1 grid. Then we can switch
-  // the current generation grid to gen2grid
+  //The next cell state should not be based on
+  // the neigbours next state.
+  // So we need two grids so that we can generate the generation 2 grid
+  // from calucations of generation 1 grid.
+  //Then we can switch the current generation grid to gen2grid
 
   private gen1Grid: Grid;
 
@@ -35,5 +37,24 @@ export class CellSimulatorEngine {
   public toggleCell = (row: number, column: number): Grid => {
     this.gen1Grid[row][column] = !this.gen1Grid[row][column];
     return this.gen1Grid;
+  };
+
+  // get sum of live cells around the coordinates. With the edges we need to wrap it to the other side
+  // using (row + height) % height
+
+  private countLiveNeighbours = (row: number, column: number): number => {
+    let liveNeighbours = 0;
+    const gridcols = this.gen1Grid[0].length;
+    const gridrows = this.gen1Grid.length;
+
+    for (let i = row - 1; i <= row + i; i++) {
+      for (let j = column - 1; j <= column + 1; j++) {
+        let col = (j + gridcols) % gridcols;
+        let row = (i + gridrows) % gridrows;
+        liveNeighbours += Number(this.gen1Grid[row][col]);
+      }
+    }
+
+    return liveNeighbours;
   };
 }
