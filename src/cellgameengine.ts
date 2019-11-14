b86999417e19ce1eb938ef9daf82c3f8fdb80527
Luan <luan.b@stjamesperth.com.au>
2019-11-14 23:55:19 +1100

    refactoring. Moving logic to a engine file

---
+++ src/cellgameengine.ts
@@ -0,0 +1,87 @@
+export type Grid = boolean[][];
+
+export class CellSimulatorEngine {
+  private gen1Grid: Grid;
+
+  private gen2Grid: Grid;
+
+  public constructor(rows: number, columns: number) {
+    // setting up the grids
+    this.gen1Grid = this.emptyGrid(rows, columns);
+    this.gen2Grid = this.emptyGrid(rows, columns);
+  }
+
+  public get cells(): Grid {
+    return this.gen1Grid;
+  }
+
+  private emptyGrid = (rows: number, columns: number): Grid => {
+    return Array(rows)
+      .fill(false)
+      .map(() => Array(columns).fill(false));
+  };
+
+  // allows to toggle the state of the cell at the board coordinates: dead -> alive or alive -> dead
+
+  public toggleCell = (row: number, column: number): Grid => {
+    this.gen1Grid[row][column] = !this.gen1Grid[row][column];
+    return this.gen1Grid;
+  };
+
+  // get sum of live cells around the coordinates. With the edges we need to wrap it to the other side
+  // using (row + height) % height
+
+  private countLiveNeighbours = (row: number, column: number): number => {
+    let liveNeighbours = 0;
+    const gridcols = this.gen1Grid[0].length;
+    const gridrows = this.gen1Grid.length;
+
+    for (let i = row - 1; i <= row + i; i++) {
+      for (let j = column - 1; j <= column + 1; j++) {
+        let col = (j + gridcols) % gridcols;
+        let row = (i + gridrows) % gridrows;
+        liveNeighbours += Number(this.gen1Grid[row][col]);
+      }
+    }
+
+    return liveNeighbours;
+  };
+
+  // Logic for the rules of the next gen cell state
+
+  // if false -> 3 live neigbours then True
+  // if true -> < 2 live neigbours or > 3 live neigbours then False
+
+  private gen2CellState = (
+    row: number,
+    column: number,
+    liveNeighbours: number
+  ): void => {
+    if (liveNeighbours === 3) {
+      this.gen2Grid[row][column] = true;
+    } else if (liveNeighbours > 3 || liveNeighbours < 2)
+      this.gen2Grid[row][column] = false;
+    else {
+      this.gen2Grid[row][column] = this.gen1Grid[row][column];
+    }
+  };
+
+  private updateGrid = (): void => {
+    const gen2 = this.gen2Grid;
+    this.gen2Grid = this.gen1Grid;
+
+    this.gen1Grid = gen2;
+  };
+
+  public iterate = (): Grid => {
+    this.gen1Grid.forEach((row: boolean[], rowIndex: number) => {
+      row.forEach((_, columnIndex: number) => {
+        const liveNeighbours = this.countLiveNeighbours(rowIndex, columnIndex);
+        this.gen2CellState(rowIndex, columnIndex, liveNeighbours);
+      });
+    });
+
+    this.updateGrid();
+    return this.gen1Grid;
+  };
+}
